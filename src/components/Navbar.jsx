87395ac78a84f208713061031d7ee45d26a376a5
asantesylvesterkwame <intelligentson17@gmail.com>
2023-05-04 11:56:04 +0000

    created all nav Elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import searchIcon  from "./search.png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="mainDiv">
+            <div className='border-none h-20 flex gap-10 items-center p-5 '>
+        <div className="LogoDiv">
+            <h1 className='text-2xl font-bold'>Magnify</h1>
+        </div>
+        <div className="searchBar">
+            <div className="SearchBox flex border p-2.5 gap-5 rounded-2xl shadow">
+            <input type="search" placeholder='What are you looking for?' className='outline-none font-light'/>
+            <img src={searchIcon} alt="searchButton" className='w-5 h-5 hover:cursor-pointer' />
+            </div>
+        </div>
+        <br />
+        
+    </div>
+    <div className="SearchNavLinks flex w-full">
+        <ul className='flex gap-5  px-5 w-full shadow-lg focus:text-sky-600 cursor-pointer'>
+            <li className=' hover:text-sky-600'>All</li>
+            <li className=' hover:text-sky-600'>Videos</li>
+            <li className=' hover:text-sky-600'>Images</li>
+            <li className=' hover:text-sky-600'>News</li>
+        </ul>
+    </div>
+</div>
+
+    
+    
   )
 }
 
