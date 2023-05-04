109c7dcb071e711171734a730a76c3940e592d2f
asantesylvesterkwame <intelligentson17@gmail.com>
2023-05-04 11:55:17 +0000

    created routes for various search components

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { Routes, BrowserRouter, Route } from 'react-router-dom'
+import AllResults from '../components/AllResults'
+import VideoResults from '../components/VideoResults'
+import ImageResults from '../components/ImageResults'
+import NewsResult from '../components/NewsResults'
 
 const MainPage = () => {
   return (
-    <div>
+    <div className='flex flex-col justify-between'>
         <Navbar/>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/all' element={<AllResults />} />
+                <Route path='/videos' element={<VideoResults/>}/>
+                <Route path='/images' element={<ImageResults/>}/>
+                <Route path='/news' element={<NewsResult/>}/>
+            </Routes>
+        </BrowserRouter>
+        <Footer/>
     </div>
   )
 }
