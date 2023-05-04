476a6cfbe7bc72458af5eac9d0836a4e890c5310
asantesylvesterkwame <intelligentson17@gmail.com>
2023-05-04 22:58:56 +0000

    implemented a function to get the value for the searc Input

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import searchIcon  from "./search.png"
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+    function SearchResults(e) {
+        e.preventDefault();
+        const searchInputValue = e.target.searchInput.value;
+        console.log(searchInputValue);
+    }
+    
   return (
     <div className="mainDiv">
             <div className='border-none h-20 flex gap-10 items-center p-5 '>
         <div className="LogoDiv">
-            <h1 className='text-2xl font-bold'>Magnify</h1>
+            <NavLink to="/"><h1 className='text-2xl font-bold'>Magnify</h1></NavLink>
         </div>
-        <div className="searchBar">
-            <div className="SearchBox flex border p-2.5 gap-5 rounded-2xl shadow">
-            <input type="search" placeholder='What are you looking for?' className='outline-none font-light'/>
-            <img src={searchIcon} alt="searchButton" className='w-5 h-5 hover:cursor-pointer' />
+        <form onSubmit={SearchResults} className="searchBar">
+            <div className="SearchBox flex border p-2.5 gap-1 rounded-2xl shadow ">
+            <input  type="search" name='searchInput' placeholder='What are you looking for?' className='outline-none font-light sm:w-full '/>
+            <button><img src={searchIcon} alt="searchButton"  className='w-5 h-5 hover:cursor-pointer' /></button>
             </div>
-        </div>
-        <br />
+        </form>
         
     </div>
     <div className="SearchNavLinks flex w-full">
-        <ul className='flex gap-5  px-5 w-full shadow-lg focus:text-sky-600 cursor-pointer'>
-            <li className=' hover:text-sky-600'>All</li>
-            <li className=' hover:text-sky-600'>Videos</li>
-            <li className=' hover:text-sky-600'>Images</li>
-            <li className=' hover:text-sky-600'>News</li>
-        </ul>
+        <div className='flex gap-5  px-5 w-full shadow-lg focus:text-sky-600 cursor-pointer'>
+            <NavLink to="/"><p className=' hover:text-sky-600'>All</p></NavLink>
+            <NavLink to="/videos"><p className=' hover:text-sky-600'>Videos</p></NavLink>
+            <NavLink to="/images"><p className=' hover:text-sky-600'>Images</p></NavLink>
+            <NavLink to="/news"><p className=' hover:text-sky-600'>News</p></NavLink>   </div>
     </div>
 </div>
 
