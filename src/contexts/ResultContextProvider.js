4ba87440d7982287d83fa22662e85eba2e1261c9
asantesylvesterkwame <intelligentson17@gmail.com>
2023-05-05 13:45:44 +0000

    added context provider

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -11,17 +11,32 @@ export const ResultsContextProvider = ({children}) =>{
     const ResultsGetter = async (type) => {
         setIsLoading(true);
 
-        const options = {
-            method: 'GET',
-            url: 'https://google-search74.p.rapidapi.com/',
-            headers: {
-              'X-RapidAPI-Key': '260dd5e289mshdb96ae4cccefbfap11a919jsn5685b58126c9',
-              'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
-            }
-          };
+        // const options = {
+        //     method: 'GET',
+        //     url: 'https://google-search74.p.rapidapi.com/',
+        //     headers: {
+        //       'X-RapidAPI-Key': '260dd5e289mshdb96ae4cccefbfap11a919jsn5685b58126c9',
+        //       'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+        //     }
+        //   };
           
 
-        const Response = await fetch(`${BaseUrl}${type}`, options )
-        console.log(Response);
+        const Response = await fetch(`${BaseUrl}${type}`, {
+          method: "GET",
+          headers: {
+            'X-RapidAPI-Key': '260dd5e289mshdb96ae4cccefbfap11a919jsn5685b58126c9',
+            'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+          }
+        } )
+        const data = await Response.json();
+        setGetResults(data) ;
+        setIsLoading(false);
     }
+    return(
+      <ResultContext.Provider value={{ ResultsGetter, searchTerm, setSearchTerm, isLoading, getresults }}>
+        {children}
+      </ResultContext.Provider>
+    )
 }
+
+export const useResultContext = () => useContext(ResultContext);
